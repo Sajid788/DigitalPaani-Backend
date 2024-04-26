4217bce0f4655373e0455ddc167c9d1011202b89
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-26 11:39:09 +0530

    BookModel created

---
+++ model/book_model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: [3, 'Title must be at least 3 characters long'],
+    maxlength: [100, 'Title cannot exceed 100 characters'],
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: [3, 'Author name must be at least 3 characters long'],
+    maxlength: [50, 'Author name cannot exceed 50 characters'],
+  },
+  publicationYear: {
+    type: Number,
+    required: true,
+    min: [1000, 'Publication year must be after year 1000'],
+    max: [new Date().getFullYear(), 'Publication year cannot be in the future'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const BookModel = model("Book", bookSchema);
+
+module.exports = {BookModel};
